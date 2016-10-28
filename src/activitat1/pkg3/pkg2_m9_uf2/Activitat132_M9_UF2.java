c7bd72e3d299250252e80cbf86aa2f28148979ec
davidblancolopez <daviddbl94@gmail.com>
2016-10-28 13:50:05 +0200

    Cambios

--- src/activitat1/pkg3/pkg2_m9_uf2/Activitat132_M9_UF2.java
+++ src/activitat1/pkg3/pkg2_m9_uf2/Activitat132_M9_UF2.java
@@ -14,7 +14,7 @@ public class Activitat132_M9_UF2 {
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         int numero;
-        int contador = 0;
+        int contador = 1;
         
         System.out.println("Introdueix un numero: ");
         numero = lector.nextInt();
@@ -27,7 +27,7 @@ public class Activitat132_M9_UF2 {
                 contador = i + 1000;
             }
             
-            for (int j = i; j < i + 1000; j++) {
+            for (int j = i; j < contador + 1000; j++) {
                 
             }
         }
