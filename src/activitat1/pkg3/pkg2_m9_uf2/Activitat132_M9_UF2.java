054ffeafbb29a5f29b8ed14eac15b786b7a73ce3
davidblancolopez <daviddbl94@gmail.com>
2016-10-28 19:44:36 +0200

    Cambios.

--- src/activitat1/pkg3/pkg2_m9_uf2/Activitat132_M9_UF2.java
+++ src/activitat1/pkg3/pkg2_m9_uf2/Activitat132_M9_UF2.java
@@ -14,14 +14,33 @@ public class Activitat132_M9_UF2 {
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         int numero;
-        int contador = 1;
+        int contador = 3;
+        int inicio, fin;
         
-        System.out.println("Introdueix un numero: ");
-        numero = lector.nextInt();
+
+        for (int i = 1; i <= contador; i++) {
+            inicio = (i - 1) * 1000;
+            fin = i * 1000;
+            Runnable fil = new Fils(inicio, fin);
+            new Thread (fil).start();
+        }
         
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(numero);
         
         
+        
+        
+        
+        
+        
+        
+        
+        
+        //ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(numero);
+        
+        
+        
+        
+        /*
         for (int i = 1; i < numero; i++) {
             if (i > 1) {
                 contador = i + 1000;
@@ -31,7 +50,7 @@ public class Activitat132_M9_UF2 {
                 executor.submit(Fils(j));
             }
         }
-        
+        */
         
         
     }
--- src/activitat1/pkg3/pkg2_m9_uf2/Fils.java
+++ src/activitat1/pkg3/pkg2_m9_uf2/Fils.java
@@ -2,27 +2,36 @@ package activitat1.pkg3.pkg2_m9_uf2;
 
 public class Fils implements Runnable {
 
-    int numComprovar;
+    
+    int inicio;
+    int fin;
 
-    public Fils(int numero) {
-        this.numComprovar = numero;
+    public Fils(int inicio, int fin) {
+        this.inicio = inicio;
+        this.fin = fin;
     }
-
-
+    
+    @Override
+    
     public void run() {
         int contador = 2;
         boolean primo = true;
-
-        while ((primo) && (contador != numComprovar)) {
-            if (numComprovar % contador == 0) {
+        
+        
+        for (int i = inicio; i <= fin; i++) {
+            System.out.println("Comprovamos " + i);
+                    while ((primo) && (contador != i)) {
+            if (i % contador == 0) {
                 primo = false;
             }
             contador++;
         }
         
         if (primo) {
-            System.out.println(numComprovar + " es un numero primo.");
+            System.out.println(i + " es un numero primo.");
         }
+        }
+
     }
 
 }
